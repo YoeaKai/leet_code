7659f6e282c57bc8b00fa25f98aa68b228aa2561
Yoea Kai <cek2llm@gmail.com>
2022-01-13 10:58:49 +0800

    add: permutations

--- application/application.go
+++ application/application.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	palindrome_partitioning "github.com/YoeaKai/leet_code/topic/palindrome_partitioning"
+	"github.com/YoeaKai/leet_code/topic/permutations"
 )
 
 func run() {
-	// fmt.Println(permutations.Permute([]int{2, 3, 1}))
-	fmt.Println(palindrome_partitioning.Partition("ababbbab"))
+	fmt.Println(permutations.Permute([]int{2, 3, 1}))
+	//fmt.Println(palindrome_partitioning.Partition("ababbbab"))
 }
 
 func main() {
--- topic/permutations/permutations.go
+++ topic/permutations/permutations.go
@@ -1,5 +1,20 @@
 package permutations
 
+func dfs(nums, tmp []int, result *[][]int) {
+	if len(nums) == 1 {
+		tmpStr := make([]int, len(tmp))
+		copy(tmpStr, tmp)
+		*result = append(*result, append(tmpStr, nums...))
+	}
+	for i, val := range nums {
+		tmpStr := make([]int, len(nums[:i]))
+		copy(tmpStr, nums[:i])
+		dfs(append(tmpStr, nums[i+1:]...), append(tmp, val), result)
+	}
+}
+
 func Permute(nums []int) [][]int {
-	return [][]int{}
+	var result [][]int
+	dfs(nums, []int{}, &result)
+	return result
 }
