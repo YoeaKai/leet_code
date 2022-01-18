e654939e6f162adf73f10fa9dab25f7c3a224060
Yoea Kai <cek2llm@gmail.com>
2022-01-18 15:20:35 +0800

    add: populating_next_right_pointers_in_each_node

---
+++ topic/populating_next_right_pointers_in_each_node/populating_next_right_pointers_in_each_node.go
@@ -0,0 +1,32 @@
+package populating_next_right_pointers_in_each_node
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func Connect(root *Node) *Node {
+	if root == nil {
+		return root
+	}
+
+	queue := []*Node{root}
+
+	for len(queue) != 0 {
+		tmpQueue := []*Node{}
+		for i, val := range queue {
+			if i != len(queue)-1 {
+				val.Next = queue[i+1]
+			}
+			if val.Left != nil {
+				tmpQueue = append(tmpQueue, val.Left)
+				tmpQueue = append(tmpQueue, val.Right)
+			}
+		}
+		queue = tmpQueue
+	}
+
+	return root
+}
