34f999d0ea878e8e8a2d8586ea972be8edf3ccf2
Yoea Kai <cek2llm@gmail.com>
2022-01-20 12:25:00 +0800

    add: longest_increasing_path_in_a_matrix

--- application/application.go
+++ application/application.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 
-	"github.com/YoeaKai/leet_code/topic/construct_binary_tree_from_preorder_and_inorder_traversal"
+	"github.com/YoeaKai/leet_code/topic/longest_increasing_path_in_a_matrix"
 )
 
 func run() {
-	//rotate_image.Rotate([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}})
+	longest_increasing_path_in_a_matrix.LongestIncreasingPath([][]int{{9, 9, 4}, {6, 6, 8}, {2, 1, 1}})
 	//println(maximize_distance_to_closest_person.MaxDistToClosest([]int{0, 0, 1, 0, 0, 1}))
-	construct_binary_tree_from_preorder_and_inorder_traversal.BuildTree([]int{3, 9, 20, 15, 7}, []int{9, 3, 15, 20, 7})
+	//construct_binary_tree_from_preorder_and_inorder_traversal.BuildTree([]int{3, 9, 20, 15, 7}, []int{9, 3, 15, 20, 7})
 }
 
 func main() {
--- topic/longest_increasing_path_in_a_matrix/longest_increasing_path_in_a_matrix.go
+++ topic/longest_increasing_path_in_a_matrix/longest_increasing_path_in_a_matrix.go
@@ -1,5 +1,55 @@
 package longest_increasing_path_in_a_matrix
 
+func findVal(x, y int, matrix, maxMap *[][]int, res, max *int) {
+	if (*maxMap)[x][y] == -1 {
+		findMax(x, y, matrix, maxMap, res)
+	}
+
+	if (*maxMap)[x][y]+1 > *max {
+		*max = (*maxMap)[x][y] + 1
+	}
+}
+
+func findMax(x, y int, matrix, maxMap *[][]int, res *int) {
+	(*maxMap)[x][y] = 0
+
+	if x > 0 && (*matrix)[x-1][y] > (*matrix)[x][y] {
+		findVal(x-1, y, matrix, maxMap, res, &(*maxMap)[x][y])
+	}
+
+	if x < len(*matrix)-1 && (*matrix)[x+1][y] > (*matrix)[x][y] {
+		findVal(x+1, y, matrix, maxMap, res, &(*maxMap)[x][y])
+	}
+
+	if y > 0 && (*matrix)[x][y-1] > (*matrix)[x][y] {
+		findVal(x, y-1, matrix, maxMap, res, &(*maxMap)[x][y])
+	}
+
+	if y < len((*matrix)[0])-1 && (*matrix)[x][y+1] > (*matrix)[x][y] {
+		findVal(x, y+1, matrix, maxMap, res, &(*maxMap)[x][y])
+	}
+}
+
 func LongestIncreasingPath(matrix [][]int) int {
-	return 1
+	res := 0
+	maxMap := make([][]int, len(matrix))
+	for i := range maxMap {
+		maxMap[i] = make([]int, len(matrix[0]))
+		for j := range maxMap[i] {
+			maxMap[i][j] = -1
+		}
+	}
+
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[0]); j++ {
+			if maxMap[i][j] == -1 {
+				findMax(i, j, &matrix, &maxMap, &res)
+			}
+			if maxMap[i][j] > res {
+				res = maxMap[i][j]
+			}
+		}
+	}
+
+	return res + 1
 }
