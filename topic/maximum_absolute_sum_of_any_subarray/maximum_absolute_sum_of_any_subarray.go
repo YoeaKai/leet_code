80afe9125b0c4bb2564105e338c1c60732885e41
Yoea Kai <cek2llm@gmail.com>
2022-02-01 11:56:28 +0800

    add: maximum_absolute_sum_of_any_subarray

---
+++ topic/maximum_absolute_sum_of_any_subarray/maximum_absolute_sum_of_any_subarray.go
@@ -0,0 +1,22 @@
+package maximum_absolute_sum_of_any_subarray
+
+// abs subarray sum
+// = one prefix sum - the other prefix sum
+// <= maximum prefix sum - minimum prefix sum
+
+// Method 1
+func MaxAbsoluteSum(nums []int) int {
+	max, min, sum := 0, 0, 0
+
+	for _, val := range nums {
+		sum += val
+
+		if sum > max {
+			max = sum
+		} else if sum < min {
+			min = sum
+		}
+	}
+
+	return max - min
+}
