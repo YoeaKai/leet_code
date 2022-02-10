519b8550eb6a283cafcf78db6360bb16a4832155
Yoea Kai <cek2llm@gmail.com>
2022-02-10 15:54:18 +0800

    add: maximal_rectangle comments

--- topic/maximal_rectangle/maximal_rectangle.go
+++ topic/maximal_rectangle/maximal_rectangle.go
@@ -18,7 +18,9 @@ func MaximalRectangle(matrix [][]byte) int {
 	res := 0
 	m, n := len(matrix), len(matrix[0])
 	hight := make([]int, n)
+	// Record the leftmost index
 	left := make([]int, n)
+	// Record the rightmost index
 	right := make([]int, n)
 
 	for i := 0; i < n; i++ {
@@ -30,6 +32,7 @@ func MaximalRectangle(matrix [][]byte) int {
 		for j := 0; j < n; j++ {
 			if matrix[i][j] == '1' {
 				hight[j] += 1
+				// max() will find the the upper left corner of the largest rectangle.
 				left[j] = max(left[j], curLeft)
 			} else {
 				hight[j] = 0
@@ -40,6 +43,7 @@ func MaximalRectangle(matrix [][]byte) int {
 			k := n - 1 - j
 
 			if matrix[i][k] == '1' {
+				// min() will find the the bottom right corner of largest rectangle.
 				right[k] = min(right[k], curRight)
 			} else {
 				right[k] = n
@@ -48,6 +52,7 @@ func MaximalRectangle(matrix [][]byte) int {
 		}
 
 		for j := 0; j < n; j++ {
+			// The largest rectangle in this index = (right[j] - left[j]) * hight[j]
 			res = max(res, (right[j]-left[j])*hight[j])
 		}
 	}
