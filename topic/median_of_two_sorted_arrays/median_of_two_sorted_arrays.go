d1ecd7a9cc179d1bbb031e42992dd9e98deb333b
Yoea Kai <cek2llm@gmail.com>
2022-02-24 17:49:56 +0800

    enhance: median_of_two_sorted_arrays method 2

--- topic/median_of_two_sorted_arrays/median_of_two_sorted_arrays.go
+++ topic/median_of_two_sorted_arrays/median_of_two_sorted_arrays.go
@@ -1,5 +1,70 @@
 package median_of_two_sorted_arrays
 
+// Method 1
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	m := len(nums1)
+	n := len(nums2)
+
+	if m > n {
+		return FindMedianSortedArrays(nums2, nums1)
+	}
+
+	left := 0
+	right := m
+
+	for left <= right {
+		i := (left + right) / 2
+		j := (m+n+1)/2 - i
+
+		if i > 0 && nums1[i-1] > nums2[j] {
+			right = i - 1
+		} else if i < m && nums2[j-1] > nums1[i] {
+			left = i + 1
+		} else {
+			leftMax := 0
+			if i == 0 {
+				leftMax = nums2[j-1]
+			} else if j == 0 {
+				leftMax = nums1[i-1]
+			} else {
+				leftMax = max(nums2[j-1], nums1[i-1])
+			}
+
+			if (m+n)&1 == 1 {
+				return float64(leftMax)
+			}
+
+			rightMin := 0
+			if i == m {
+				rightMin = nums2[j]
+			} else if j == n {
+				rightMin = nums1[i]
+			} else {
+				rightMin = min(nums2[j], nums1[i])
+			}
+
+			return float64(leftMax+rightMin) / 2
+		}
+	}
+
+	return 0.0
+}
+
+// Method 2
 func findMergedArr(nums1 []int, nums2 []int) []int {
 	pos := 0
 	head := 0
@@ -49,7 +114,7 @@ func findMergedArr(nums1 []int, nums2 []int) []int {
 	return nums1
 }
 
-func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+func FindMedianSortedArrays2(nums1 []int, nums2 []int) float64 {
 	var resultArr []int
 
 	if nums1 == nil {
