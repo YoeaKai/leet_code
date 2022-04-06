e1de3f756af5a5a7408a2d8b40a053f23f45f90a
Yoea Kai <cek2llm@gmail.com>
2022-04-06 23:54:23 +0800

    Add: remove_duplicates_from_sorted_list_II

--- application/application.go
+++ application/application.go
@@ -3,16 +3,16 @@ package main
 import (
 	"fmt"
 
-	"github.com/YoeaKai/leet_code/topic/remove_duplicates_from_sorted_list"
+	"github.com/YoeaKai/leet_code/topic/remove_duplicates_from_sorted_list_II"
 )
 
 func run() {
-	e := remove_duplicates_from_sorted_list.ListNode{Val: 3, Next: nil}
-	d := remove_duplicates_from_sorted_list.ListNode{Val: 3, Next: &e}
-	c := remove_duplicates_from_sorted_list.ListNode{Val: 2, Next: &d}
-	b := remove_duplicates_from_sorted_list.ListNode{Val: 1, Next: &c}
-	a := remove_duplicates_from_sorted_list.ListNode{Val: 1, Next: &b}
-	println(remove_duplicates_from_sorted_list.DeleteDuplicates(&a))
+	e := remove_duplicates_from_sorted_list_II.ListNode{Val: 3, Next: nil}
+	d := remove_duplicates_from_sorted_list_II.ListNode{Val: 3, Next: &e}
+	c := remove_duplicates_from_sorted_list_II.ListNode{Val: 2, Next: &d}
+	b := remove_duplicates_from_sorted_list_II.ListNode{Val: 1, Next: &c}
+	a := remove_duplicates_from_sorted_list_II.ListNode{Val: 1, Next: &b}
+	println(remove_duplicates_from_sorted_list_II.DeleteDuplicates(&a))
 }
 
 func main() {
---
+++ topic/remove_duplicates_from_sorted_list_II/remove_duplicates_from_sorted_list_II.go
@@ -0,0 +1,27 @@
+package remove_duplicates_from_sorted_list_II
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func DeleteDuplicates(head *ListNode) *ListNode {
+	prev := &ListNode{Next: head}
+	res := prev
+
+	for prev.Next != nil {
+		removed := false
+
+		for prev.Next.Next != nil && prev.Next.Next.Val == prev.Next.Val {
+			prev.Next = prev.Next.Next
+			removed = true
+		}
+
+		if removed {
+			prev.Next = prev.Next.Next
+		} else {
+			prev = prev.Next
+		}
+	}
+	return res.Next
+}
