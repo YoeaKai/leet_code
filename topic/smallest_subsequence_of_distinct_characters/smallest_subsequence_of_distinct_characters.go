a825a3333b7a61c8cb6e4990b4efbf5e17d5a006
Yoea Kai <cek2llm@gmail.com>
2022-04-07 16:55:26 +0800

    Add: smallest_subsequence_of_distinct_characters

---
+++ topic/smallest_subsequence_of_distinct_characters/smallest_subsequence_of_distinct_characters.go
@@ -0,0 +1,28 @@
+// This question is the same as 316: https://leetcode.com/problems/remove-duplicate-letters/
+
+package smallest_subsequence_of_distinct_characters
+
+func SmallestSubsequence(s string) string {
+	last := make([]int, 26)
+	for i := 0; i < len(s); i++ {
+		last[s[i]-'a'] = i
+	}
+
+	var res []rune
+	exist := make([]bool, 26)
+
+	for i, c := range s {
+		// Remove the character it will appear later and it's larger than current character.
+		for len(res) != 0 && !exist[c-'a'] && res[len(res)-1] > c && last[res[len(res)-1]-'a'] > i {
+			exist[res[len(res)-1]-'a'] = false
+			res = res[:len(res)-1]
+		}
+
+		if !exist[c-'a'] {
+			res = append(res, c)
+			exist[c-'a'] = true
+		}
+	}
+
+	return string(res)
+}
