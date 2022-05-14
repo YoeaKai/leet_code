681445e9c7ec511e59aa1a4a369e00f81e948674
Yoea Kai <cek2llm@gmail.com>
2022-05-15 00:07:58 +0800

    Fix: enhance coding logic for interleaving_string.

--- topic/interleaving_string/interleaving_string.go
+++ topic/interleaving_string/interleaving_string.go
@@ -9,19 +9,11 @@ func IsInterleave(s1 string, s2 string, s3 string) bool {
 	dp := make([]bool, len(s2)+1)
 	dp[0] = true
 
-	for j := 1; j <= len(s2); j++ {
-		dp[j] = false
-		if !dp[j] && j != 0 && s2[j-1] == s3[j-1] {
-			dp[j] = dp[j-1]
-		}
-	}
-
-	for i := 1; i <= len(s1); i++ {
+	for i := 0; i <= len(s1); i++ {
 		for j := 0; j <= len(s2); j++ {
-			pre := dp[j]
-			dp[j] = false
-			if i != 0 && s1[i-1] == s3[i+j-1] {
-				dp[j] = pre
+			// If s1[i-1] is equal to s3[i+j-1], then dp[j] is unchanged.
+			if i != 0 && s1[i-1] != s3[i+j-1] {
+				dp[j] = false
 			}
 			if !dp[j] && j != 0 && s2[j-1] == s3[i+j-1] {
 				dp[j] = dp[j-1]
