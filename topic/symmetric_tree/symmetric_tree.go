45012f4c4a4d22e014d1ad94de51edf2a72b0f43
Yoea Kai <cek2llm@gmail.com>
2022-05-17 10:09:13 +0800

    Add: symmetric_tree

--- topic/symmetric_tree/symmetric_tree.go
+++ topic/symmetric_tree/symmetric_tree.go
@@ -7,5 +7,17 @@ type TreeNode struct {
 }
 
 func IsSymmetric(root *TreeNode) bool {
-	return true
+	return isSymmetricAlgo(root.Left, root.Right)
+}
+
+func isSymmetricAlgo(left, right *TreeNode) bool {
+	if left == nil || right == nil {
+		return left == right
+	}
+
+	if left.Val != right.Val {
+		return false
+	}
+
+	return isSymmetricAlgo(left.Left, right.Right) && isSymmetricAlgo(left.Right, right.Left)
 }
