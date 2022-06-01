36b87d36e4e0d5b9f8bc36b0b3fd2b25c5436bdf
Yoea Kai <cek2llm@gmail.com>
2022-06-01 16:58:18 +0800

    Add: make_array_strictly_increasing

--- application/application.go
+++ application/application.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/YoeaKai/leet_code/topic/contiguous_array"
+	"github.com/YoeaKai/leet_code/topic/make_array_strictly_increasing"
 )
 
 func run() {
-	println(contiguous_array.FindMaxLength([]int{0, 1, 1, 0, 1, 1, 1, 0}))
+	println(make_array_strictly_increasing.MakeArrayIncreasing([]int{1, 5, 3, 6, 7}, []int{1, 3, 2, 4}))
 }
 
 func main() {
---
+++ topic/make_array_strictly_increasing/make_array_strictly_increasing.go
@@ -0,0 +1,82 @@
+package make_array_strictly_increasing
+
+import "sort"
+
+const MaxUint = ^uint(0)
+const MaxInt = int(MaxUint >> 1)
+
+func MakeArrayIncreasing(arr1 []int, arr2 []int) int {
+	sort.Ints(arr2)
+	dp := make(map[int]int)
+	dp[-1] = 0
+
+	for _, v1 := range arr1 {
+		tmp := make(map[int]int)
+
+		for key, val := range dp {
+			if v1 > key {
+				var lastCount int
+
+				if t, ok := tmp[v1]; ok {
+					lastCount = t
+				} else {
+					lastCount = MaxInt
+				}
+
+				tmp[v1] = min(lastCount, val)
+			}
+
+			i := binarySearch(&arr2, key)
+
+			if i != len(arr2) {
+				var lastCount int
+
+				if t, ok := tmp[arr2[i]]; ok {
+					lastCount = t
+				} else {
+					lastCount = MaxInt
+				}
+
+				tmp[arr2[i]] = min(lastCount, val+1)
+			}
+		}
+
+		dp = tmp
+	}
+
+	if len(dp) == 0 {
+		return -1
+	} else {
+		ret := MaxInt
+
+		for _, v := range dp {
+			ret = min(ret, v)
+		}
+
+		return ret
+	}
+}
+
+func binarySearch(arr *[]int, val int) int {
+	left := 0
+	right := len(*arr)
+
+	for left < right {
+		mid := (left + right) / 2 // left + (right - left) / 2
+
+		if (*arr)[mid] <= val {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+
+	return left
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
