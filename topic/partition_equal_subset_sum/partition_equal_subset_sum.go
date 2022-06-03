38431d5d525b92c57edfcc4e9c7e7ce9fbd9c962
Yoea Kai <cek2llm@gmail.com>
2022-06-03 20:50:45 +0800

    Fix: enhance partition_equal_subset_sum 2

--- topic/partition_equal_subset_sum/partition_equal_subset_sum.go
+++ topic/partition_equal_subset_sum/partition_equal_subset_sum.go
@@ -17,13 +17,44 @@ func CanPartition(nums []int) bool {
 		return true
 	}
 
+	dp := make([]bool, sum+1)
+	dp[0] = true
+
+	for _, num := range nums {
+		for i := sum; i > 0; i-- {
+			if i >= num {
+				dp[i] = dp[i] || dp[i-num]
+			}
+		}
+	}
+
+	return dp[sum]
+}
+
+func CanPartition2(nums []int) bool {
+	sum := 0
+
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+	}
+
+	if sum%2 == 1 {
+		return false
+	}
+
+	sum /= 2 // One part of the sum that can make the total sum.
+
+	if nums[0] == sum {
+		return true
+	}
+
 	p := make(map[int]bool)
 	p[nums[0]] = true
 
 	for i := 1; i < len(nums); i++ {
 		tmp := []int{}
 
-		for key, _ := range p {
+		for key := range p {
 			added := key + nums[i]
 
 			if added == sum {
