868fe3208c1e6a1eb05e730c55c72bc645f1c401
Yoea Kai <cek2llm@gmail.com>
2022-06-13 17:28:57 +0800

    Add: C++ environment

---
+++ application/application.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int test = 1;
+
+    if (test == 1){
+        cout << "Success" << endl;
+    } else {
+        cout << "Fail" << endl;
+    }
+}
