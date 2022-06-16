620fa27214127ae3a78b20fdf38f65a0e05148de
Yoea Kai <cek2llm@gmail.com>
2022-06-16 17:59:14 +0800

    Add: ones_and_zeroes

--- application/cpp/application.cpp
+++ application/cpp/application.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <vector>
-#include "../../topic/convert_sorted_array_to_binary_search_tree/convert_sorted_array_to_binary_search_tree.cpp"
+#include "../../topic/ones_and_zeroes/ones_and_zeroes.cpp"
 using namespace std;
 
 int main()
 {
     Solution s;
-    vector<int> p = {-10, -3, 0, 5, 9};
+    vector<string> strs = {"10", "0001", "111001", "1", "0"};
+    vector<string> strs2 = {"10", "0", "1"};
+
     cout << "------start------" << endl;
-    cout << s.sortedArrayToBST(p) << endl;
+    cout << s.findMaxForm(strs, 5, 3) << endl;
+    cout << s.findMaxForm(strs2, 1, 1) << endl;
     cout << "------over------" << endl;
 }
