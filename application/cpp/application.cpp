f4ade0042870c1589919091e17cbfa793554f83b
Yoea Kai <cek2llm@gmail.com>
2022-06-16 12:17:40 +0800

    Fix: enhance convert_sorted_array_to_binary_search_tree 3 and add macOS application ignore

--- application/cpp/application.cpp
+++ application/cpp/application.cpp
@@ -3,8 +3,11 @@
 #include "../../topic/convert_sorted_array_to_binary_search_tree/convert_sorted_array_to_binary_search_tree.cpp"
 using namespace std;
 
-int main() {
+int main()
+{
     Solution s;
-    vector<int> p = {-10,-3,0,5,9};
+    vector<int> p = {-10, -3, 0, 5, 9};
+    cout << "------start------" << endl;
     cout << s.sortedArrayToBST(p) << endl;
+    cout << "------over------" << endl;
 }
--- topic/convert_sorted_array_to_binary_search_tree/convert_sorted_array_to_binary_search_tree.cpp
+++ topic/convert_sorted_array_to_binary_search_tree/convert_sorted_array_to_binary_search_tree.cpp
@@ -1,7 +1,8 @@
 #include <vector>
-#include <cstring>
+using namespace std;
 
-struct TreeNode {
+struct TreeNode
+{
     int val;
     TreeNode *left;
     TreeNode *right;
@@ -10,24 +11,51 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    TreeNode* sortedArrayToBST(std::vector<int>& nums) {
+    using iter = vector<int>::const_iterator;
+
+    TreeNode *sortedArrayToBST(vector<int> &nums)
+    {
+        return buildBST(nums.begin(), nums.end());
+    }
+
+    TreeNode *buildBST(iter b, iter e)
+    {
+        if (b >= e)
+            return nullptr;
+
+        iter m = b + (e - b) / 2;
+
+        TreeNode *node = new TreeNode(*m);
+        node->left = buildBST(b, m);
+        node->right = buildBST(m + 1, e);
+
+        return node;
+    }
+
+    TreeNode *sortedArrayToBST2(vector<int> &nums)
+    {
         int len = nums.size();
 
-        if (len == 1){
+        if (len == 0)
+        {
+            return NULL;
+        }
+
+        if (len == 1)
+        {
             return new TreeNode(nums[0]);
         }
-        
-        int mid = len/2;
 
-        TreeNode* node = new TreeNode(nums[mid]);
+        int mid = len / 2;
 
-        vector<int> a (nums.begin(), nums.begin()+mid);
-        vector<int> b (nums.begin()+mid+1, nums.end());
-
-        node -> left = sortedArrayToBST(a);
-        node -> right = sortedArrayToBST(b);
+        TreeNode *node = new TreeNode(nums[mid]);
+        vector<int> left(nums.begin(), nums.begin() + mid);
+        vector<int> right(nums.begin() + mid + 1, nums.end());
+        node->left = sortedArrayToBST2(left);
+        node->right = sortedArrayToBST2(right);
 
         return node;
     }
