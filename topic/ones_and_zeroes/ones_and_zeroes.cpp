ae12b0c08be7cb79101db240702babb0e1349bf9
Yoea Kai <cek2llm@gmail.com>
2022-06-18 13:52:32 +0800

    Fix: enhance ones_and_zeroes

--- topic/ones_and_zeroes/ones_and_zeroes.cpp
+++ topic/ones_and_zeroes/ones_and_zeroes.cpp
@@ -13,8 +13,7 @@ public:
 		for (auto &s : strs)
 		{
 			numZeroes = numOnes = 0;
-			// count number of zeroes and ones in current string
-			for (auto c : s)
+			for (auto &c : s)
 			{
 				if (c == '0')
 					numZeroes++;
@@ -22,19 +21,9 @@ public:
 					numOnes++;
 			}
 
-			// memo[i][j] = the max number of strings that can be formed with i 0's and j 1's
-			// from the first few strings up to the current string s
-			// Catch: have to go from bottom right to top left
-			// Why? If a cell in the memo is updated(because s is selected),
-			// we should be adding 1 to memo[i][j] from the previous iteration (when we were not considering s)
-			// If we go from top left to bottom right, we would be using results from this iteration => overcounting
 			for (int i = m; i >= numZeroes; i--)
-			{
 				for (int j = n; j >= numOnes; j--)
-				{
 					memo[i][j] = max(memo[i][j], memo[i - numZeroes][j - numOnes] + 1);
-				}
-			}
 		}
 		return memo[m][n];
 	}
