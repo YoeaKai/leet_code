f1e73f36c7e705981feb92336f8f04480b80b45e
Yoea Kai <cek2llm@gmail.com>
2022-06-22 11:49:21 +0800

    Add: find_two_nonOverlapping_subArrays_each_with_target_sum algorithm

--- application/cpp/application.cpp
+++ application/cpp/application.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 #include <vector>
-#include "../../topic/maximum_width_of_binary_tree/maximum_width_of_binary_tree.cpp"
+#include "../../topic/find_two_nonOverlapping_subArrays_each_with_target_sum/find_two_nonOverlapping_subArrays_each_with_target_sum.cpp"
 using namespace std;
 
 int main()
 {
     Solution s;
-    TreeNode e = TreeNode(5);
-    TreeNode d = TreeNode(4);
-    TreeNode c = TreeNode(3, nullptr, &e);
-    TreeNode b = TreeNode(2, &d, nullptr);
-    TreeNode a = TreeNode(1, &b, &c);
+    vector<int> v = {1, 3, 1};
 
     cout << "------start------" << endl;
-    cout << s.widthOfBinaryTree(&a) << endl;
+    cout << s.minSumOfLengths(v, 4) << endl;
     cout << "------over------" << endl;
 }
--- topic/find_two_nonOverlapping_subArrays_each_with_target_sum/find_two_nonOverlapping_subArrays_each_with_target_sum.cpp
+++ topic/find_two_nonOverlapping_subArrays_each_with_target_sum/find_two_nonOverlapping_subArrays_each_with_target_sum.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 class Solution
@@ -6,6 +7,31 @@ class Solution
 public:
 	int minSumOfLengths(vector<int> &arr, int target)
 	{
-		return 1;
+		int ret = INT_MAX, dis = INT_MAX, sum = 0;
+		unordered_map<int, int> mp;
+		mp[0] = -1;
+
+		for (int i = 0; i < arr.size(); i++)
+		{
+			sum += arr[i];
+			mp[sum] = i;
+		}
+
+		sum = 0;
+
+		for (int i = 0; i < arr.size(); i++)
+		{
+			sum += arr[i];
+
+			if (mp.find(sum - target) != mp.end())
+				if (i - mp[sum - target] < dis)
+					dis = i - mp[sum - target];
+
+			// mp.find(sum + target) searches for any sub-array starting with index i+1 with sum target.
+			if (mp.find(sum + target) != mp.end() && dis != INT_MAX)
+				ret = min(ret, mp[sum + target] - i + dis);
+		}
+
+		return ret == INT_MAX ? -1 : ret;
 	}
 };
