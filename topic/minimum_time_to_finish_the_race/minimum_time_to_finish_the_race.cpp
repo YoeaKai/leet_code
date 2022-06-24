f0f5a17f6d6ce296e9bf6b88c0ecfe634fc3b423
Yoea Kai <cek2llm@gmail.com>
2022-06-24 17:18:53 +0800

    Fix: minimum_time_to_finish_the_race little optimize

--- topic/minimum_time_to_finish_the_race/minimum_time_to_finish_the_race.cpp
+++ topic/minimum_time_to_finish_the_race/minimum_time_to_finish_the_race.cpp
@@ -27,19 +27,15 @@ public:
 		vector<vector<int> > without_change(n, vector<int>(20, limit));
 		for (int i = 0; i < n; i++)
 		{
-			without_change[i][1] = tires[i][0];
+			int tmp = tires[i][0] = without_change[i][1] = tires[i][0];
+
 			for (int j = 2; j < 20; j++)
 			{
 				if ((long long)without_change[i][j - 1] * tires[i][1] >= limit)
 					break;
-				without_change[i][j] = without_change[i][j - 1] * tires[i][1];
-			}
-			// since we define it as the total time, rather than just the time for the j-th lap
-			// we have to make it prefix sum
-			for (int j = 2; j < 20; j++)
-			{
-				if ((long long)without_change[i][j - 1] + without_change[i][j] >= limit)
-					break;
+				without_change[i][j] = tmp * tires[i][1];
+				tmp = without_change[i][j];
+				// since we define it as the total time, rather than just the time for the j-th lap we have to make it prefix sum.
 				without_change[i][j] += without_change[i][j - 1];
 			}
 		}
