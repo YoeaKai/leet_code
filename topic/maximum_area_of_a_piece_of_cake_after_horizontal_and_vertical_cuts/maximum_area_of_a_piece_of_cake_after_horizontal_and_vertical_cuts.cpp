a517c0e61badd35344c841860a8d5902e61d9627
Yoea Kai <cek2llm@gmail.com>
2022-07-03 01:38:17 +0800

    Fix: maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts enhance

--- topic/maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts/maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts.cpp
+++ topic/maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts/maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts.cpp
@@ -3,36 +3,22 @@ using namespace std;
 
 class Solution
 {
+	int getMax(int &edge, vector<int> &cuts)
+	{
+		sort(cuts.begin(), cuts.end());
+
+		int len = cuts.size();
+		int maxVal = max(edge - cuts[len - 1], cuts[0]);
+
+		for (int i = 1; i < len; i++)
+			maxVal = max(maxVal, cuts[i] - cuts[i - 1]);
+
+		return maxVal;
+	}
+
 public:
 	int maxArea(int h, int w, vector<int> &horizontalCuts, vector<int> &verticalCuts)
 	{
-		sort(horizontalCuts.begin(), horizontalCuts.end());
-		sort(verticalCuts.begin(), verticalCuts.end());
-
-		int heightMax = horizontalCuts[0], weightMax = verticalCuts[0], i = 1;
-
-		for (; i < horizontalCuts.size(); i++)
-		{
-			horizontalCuts[i - 1] = horizontalCuts[i] - horizontalCuts[i - 1];
-
-			if (horizontalCuts[i - 1] > heightMax)
-				heightMax = horizontalCuts[i - 1];
-		}
-
-		if (h - horizontalCuts[i - 1] > heightMax)
-			heightMax = h - horizontalCuts[i - 1];
-
-		for (i = 1; i < verticalCuts.size(); i++)
-		{
-			verticalCuts[i - 1] = verticalCuts[i] - verticalCuts[i - 1];
-
-			if (verticalCuts[i - 1] > weightMax)
-				weightMax = verticalCuts[i - 1];
-		}
-
-		if (w - verticalCuts[i - 1] > weightMax)
-			weightMax = w - verticalCuts[i - 1];
-
-		return ((long)heightMax * weightMax) % 1000000007;
+		return (long)getMax(h, horizontalCuts) * getMax(w, verticalCuts) % 1000000007;
 	}
 };
