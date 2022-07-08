dff21ecf176bc8ac5383bd7f36a64815118cafcb
Yoea Kai <cek2llm@gmail.com>
2022-07-08 17:47:18 +0800

    Add: best_time_to_buy_and_sell_stock_with_cooldown

--- application/cpp/application.cpp
+++ application/cpp/application.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <vector>
-#include "../../topic/longest_increasing_subsequence/longest_increasing_subsequence.cpp"
+#include "../../topic/best_time_to_buy_and_sell_stock_with_cooldown/best_time_to_buy_and_sell_stock_with_cooldown.cpp"
 using namespace std;
 
 int main()
 {
     Solution s;
-    vector<int> a = {0, 1, -1, 3, 2, 3};
+    vector<int> a = {2, 3, 0, 10, 2};
 
     cout << "------start------" << endl;
-    cout << s.lengthOfLIS(a) << endl;
+    cout << s.maxProfit(a) << endl;
     cout << "------over------" << endl;
 }
