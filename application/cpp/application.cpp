a2fa05937fa8b0948f2626a9a1723170d1fd7c40
Yoea Kai <cek2llm@gmail.com>
2022-07-13 15:44:08 +0800

    Add: best_time_to_buy_and_sell_stock_with_cooldown algorithm

--- application/cpp/application.cpp
+++ application/cpp/application.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <vector>
-#include "../../topic/matchsticks_to_square/matchsticks_to_square.cpp"
+#include "../../topic/best_time_to_buy_and_sell_stock_with_cooldown/best_time_to_buy_and_sell_stock_with_cooldown.cpp"
 using namespace std;
 
 int main()
 {
     Solution s;
-    vector<int> a = {1, 1, 2, 2, 2};
+    vector<int> a = {1, 2, 3, 0, 2};
 
     cout << "------start------" << endl;
-    cout << s.makesquare(a) << endl;
+    cout << s.maxProfit(a) << endl;
     cout << "------over------" << endl;
 }
--- topic/best_time_to_buy_and_sell_stock_with_cooldown/best_time_to_buy_and_sell_stock_with_cooldown.cpp
+++ topic/best_time_to_buy_and_sell_stock_with_cooldown/best_time_to_buy_and_sell_stock_with_cooldown.cpp
@@ -6,6 +6,30 @@ class Solution
 public:
 	int maxProfit(vector<int> &prices)
 	{
-		return 2;
+		// Three state
+		int buyed = -prices[0], beforeCooldown = INT_MIN, afterCooldown = 0, preBuyed;
+
+		for (int i = 1; i < prices.size(); i++)
+		{
+			preBuyed = buyed;
+
+			buyed = max(preBuyed, afterCooldown - prices[i]);
+			afterCooldown = max(beforeCooldown, afterCooldown);
+			beforeCooldown = preBuyed + prices[i];
+		}
+
+		// From this:
+		// for (int i = 1; i < prices.size(); i++)
+		// {
+		// 	preBuyed = buyed;
+		// 	preBeforeCooldown = beforeCooldown;
+		// 	preAfterCooldown = afterCooldown;
+
+		// 	buyed = max(preBuyed, preAfterCooldown - prices[i]);
+		// 	beforeCooldown = preBuyed + prices[i];
+		// 	afterCooldown = max(preBeforeCooldown, preAfterCooldown);
+		// }
+
+		return max(beforeCooldown, afterCooldown);
 	}
 };
