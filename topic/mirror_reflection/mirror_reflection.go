1984b8377f5cea786fe4dacb22716a8ffccae42f
Yoea Kai <cek2llm@gmail.com>
2022-08-22 17:10:00 +0800

    Add: mirror_reflection algorithm

--- topic/mirror_reflection/mirror_reflection.go
+++ topic/mirror_reflection/mirror_reflection.go
@@ -6,13 +6,17 @@ func MirrorReflection(p int, q int) int {
 		q /= 2
 	}
 
-	if q == 0 {
-		return 0
+	if p%2 == 1 {
+		if q%2 == 0 {
+			return 0
+		} else {
+			return 1
+		}
 	}
 
-	if p == 1 {
-		return 1
+	if p%2 == 0 && q%2 == 1 {
+		return 2
 	}
 
-	return 2
+	return -1
 }
