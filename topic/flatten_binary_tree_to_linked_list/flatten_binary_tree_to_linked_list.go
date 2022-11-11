e77d3ad6a53e58aa3034d3d8bbbde658d34ba4ea
YoeaKai <cek2llm@gmail.com>
2022-11-12 00:10:55 +0800

    Add: flatten_binary_tree_to_linked_list method 2

--- topic/flatten_binary_tree_to_linked_list/flatten_binary_tree_to_linked_list.go
+++ topic/flatten_binary_tree_to_linked_list/flatten_binary_tree_to_linked_list.go
@@ -6,21 +6,43 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-var pre *TreeNode
-
+// Method 1
 func Flatten(root *TreeNode) {
-	pre = nil
+	flattenTree(root, nil)
+}
+
+func flattenTree(root, pre *TreeNode) *TreeNode {
+	if root == nil {
+		if pre != nil {
+			return pre
+		}
+		return nil
+	}
+
+	right := flattenTree(root.Right, pre)
+	left := flattenTree(root.Left, right)
+	root.Right = left
+	root.Left = nil
+
+	return root
+}
+
+// Method 2: with global variable.
+var pre2 *TreeNode
+
+func Flatten2(root *TreeNode) {
+	pre2 = nil
 	flattenTree(root)
 }
 
-func flattenTree(root *TreeNode) {
+func flattenTree2(root *TreeNode) {
 	if root == nil {
 		return
 	}
 
 	flattenTree(root.Right)
 	flattenTree(root.Left)
-	root.Right = pre
+	root.Right = pre2
 	root.Left = nil
-	pre = root
+	pre2 = root
 }
