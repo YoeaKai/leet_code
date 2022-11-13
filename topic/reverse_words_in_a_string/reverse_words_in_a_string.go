6052ca3fcc7ad6702cb97f75cdfd4c0da7d9d4ff
YoeaKai <cek2llm@gmail.com>
2022-11-13 11:31:30 +0800

    Add: reverse_words_in_a_string

---
+++ topic/reverse_words_in_a_string/reverse_words_in_a_string.go
@@ -0,0 +1,26 @@
+package reverse_words_in_a_string
+
+func ReverseWords(s string) string {
+	res := []string{}
+	idx := len(s) - 1
+
+	for idx >= 0 {
+		for idx >= 0 && s[idx] == ' ' {
+			idx--
+		}
+
+		if idx < 0 {
+			break
+		}
+
+		head := idx - 1
+		for head >= 0 && s[head] != ' ' {
+			head--
+		}
+
+		res = append(res, s[head+1:idx+1])
+		idx = head
+	}
+
+	return strings.Join(res, " ")
+}
