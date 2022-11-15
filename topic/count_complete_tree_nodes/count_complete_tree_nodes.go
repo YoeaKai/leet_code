2e3c8c5cd217cb4649f6ec3f458d668b008fa188
YoeaKai <cek2llm@gmail.com>
2022-11-15 22:19:29 +0800

    Add: count_complete_tree_nodes algorithm

--- topic/count_complete_tree_nodes/count_complete_tree_nodes.go
+++ topic/count_complete_tree_nodes/count_complete_tree_nodes.go
@@ -6,6 +6,28 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+func height(node *TreeNode) int {
+	if node == nil {
+		return -1
+	}
+
+	return 1 + height((*node).Left)
+}
+
 func CountNodes(root *TreeNode) int {
-	return 0
+	nodes := 0
+	h := height(root)
+
+	for root != nil {
+		if height(root.Right) == h-1 {
+			nodes += 1 << h
+			root = root.Right
+		} else {
+			nodes += 1 << (h - 1)
+			root = root.Left
+		}
+		h--
+	}
+
+	return nodes
 }
