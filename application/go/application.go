2343ccd4e3d92eb724ef128c87896d304d730132
YoeaKai <cek2llm@gmail.com>
2022-12-01 23:31:39 +0800

    Add: determine_if_string_halves_are_alike algorithm

--- application/go/application.go
+++ application/go/application.go
@@ -3,13 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/YoeaKai/leet_code/topic/find_players_with_zero_or_one_losses"
+	"github.com/YoeaKai/leet_code/topic/determine_if_string_halves_are_alike"
 )
 
 func run() {
-	println(find_players_with_zero_or_one_losses.FindWinners(
-		[][]int{{1, 3}, {2, 3}, {3, 6}, {5, 6}, {5, 7}, {4, 5}, {4, 8}, {4, 9}, {10, 4}, {10, 9}},
-	))
+	println(determine_if_string_halves_are_alike.HalvesAreAlike("book"))
 }
 
 func main() {
--- topic/determine_if_string_halves_are_alike/determine_if_string_halves_are_alike.go
+++ topic/determine_if_string_halves_are_alike/determine_if_string_halves_are_alike.go
@@ -1,5 +1,22 @@
 package determine_if_string_halves_are_alike
 
+import "strings"
+
 func HalvesAreAlike(s string) bool {
-	return true
+	var vowels int
+	s = strings.ToLower(s)
+
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] == 97 || s[i] == 101 || s[i] == 105 || s[i] == 111 || s[i] == 117 {
+			vowels++
+		}
+	}
+
+	for i := len(s) / 2; i < len(s); i++ {
+		if s[i] == 97 || s[i] == 101 || s[i] == 105 || s[i] == 111 || s[i] == 117 {
+			vowels--
+		}
+	}
+
+	return vowels == 0
 }
