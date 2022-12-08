ad32589e9aeafad5aa1e95cd06c45f7dc85819c3
Yoea Kai <cek2llm@gmail.com>
2022-12-08 17:53:56 +0800

    Add: Python application

---
+++ py_run.py
@@ -0,0 +1,12 @@
+from topic.two_sum.py.two_sum import Solution
+
+
+def run():
+    solution = Solution()
+    print(solution.twoSum([2, 7, 11, 15], 9))
+
+
+if __name__ == '__main__':
+    print("----------Start----------")
+    run()
+    print("----------Finish----------")
