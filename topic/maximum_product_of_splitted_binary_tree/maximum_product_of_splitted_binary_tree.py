221f11ff21819cafcab6fc9f9591e55b22b0cc05
YoeaKai <cek2llm@gmail.com>
2022-12-10 23:59:41 +0800

    Add: maximum_difference_between_node_and_ancestor

--- topic/maximum_product_of_splitted_binary_tree/maximum_product_of_splitted_binary_tree.py
+++ topic/maximum_product_of_splitted_binary_tree/maximum_product_of_splitted_binary_tree.py
@@ -8,15 +8,4 @@ class TreeNode:
 
 class Solution:
 	def maxProduct(self, root: Optional[TreeNode]) -> int:
-		self.res = total = 0
-		
-		def s(root):
-			if not root: 
-				return 0
-			left, right = s(root.left), s(root.right)
-			self.res = max(self.res, left * (total - left), right * (total - right))
-			return left + right + root.val
-
-		total = s(root)
-		s(root)
-		return self.res % (10**9 + 7)
+		return 1
