0de52884e9dae5981fd6404bc97489e5223f72d5
YoeaKai <cek2llm@gmail.com>
2022-12-12 00:37:17 +0800

    Add: single_number

---
+++ topic/single_number/single_number.py
@@ -0,0 +1,8 @@
+from typing import List
+from functools import reduce
+
+class Solution:
+	def singleNumber(self, nums: List[int]) -> int:
+		# If you XOR same numbers it will return zero.
+		# Refer: https://thepythonguru.com/python-builtin-functions/reduce/
+		return reduce(lambda total, el: total ^ el, nums)
