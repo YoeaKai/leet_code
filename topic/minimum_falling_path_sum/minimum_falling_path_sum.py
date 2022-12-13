6e8dfc3a32012e3bf8477a419b68da6223ebbe27
YoeaKai <cek2llm@gmail.com>
2022-12-13 23:20:54 +0800

    Add: minimum_falling_path_sum algorithm

--- topic/minimum_falling_path_sum/minimum_falling_path_sum.py
+++ topic/minimum_falling_path_sum/minimum_falling_path_sum.py
@@ -3,4 +3,7 @@ from typing import List
 
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        return 1
+        for i in range(1, len(matrix)):
+            for j in range(len(matrix[i])):
+                matrix[i][j] += min(matrix[i - 1][max(0, j - 1):j + 2])
+        return min(matrix[-1])
