c65b750a12eb08d20f4138f8b246994a51d49641
YoeaKai <cek2llm@gmail.com>
2022-12-17 23:13:09 +0800

    Add: evaluate_reverse_polish_notation

---
+++ topic/evaluate_reverse_polish_notation/evaluate_reverse_polish_notation.py
@@ -0,0 +1,44 @@
+from typing import List
+from typing import operator
+
+class Solution:
+    def __init__(self):
+        self.operators = {
+            '+': lambda y, x: x + y,
+            '-': lambda y, x: x - y,
+            '*': lambda y, x: x * y,
+            '/': lambda y, x: int(operator.truediv(x, y))
+        }
+
+    # Solution Using lambda.
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+
+        for token in tokens:
+            if token in self.operators:
+                stack.append(self.operators[token](stack.pop(), stack.pop()))
+            else:
+                stack.append(int(token))
+
+        return stack[0]
+
+    # Intuitive solution.
+    def evalRPN2(self, tokens: List[str]) -> int:
+        stack = []
+
+        for token in tokens:
+            if token not in "+-*/":
+                stack.append(int(token))
+            else:
+                val1, val2 = stack.pop(), stack.pop()
+                if token == "+":
+                    stack.append(val1+val2)
+                elif token == "-":
+                    stack.append(val2-val1)
+                elif token == "*":
+                    stack.append(val1*val2)
+                else:
+                    stack.append(int(val2/val1))
+
+        return stack.pop()
+        
