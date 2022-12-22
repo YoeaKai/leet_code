297abb5edb6db535614e54793226f9a7873b26b1
YoeaKai <cek2llm@gmail.com>
2022-12-22 23:23:54 +0800

    Add: single_number_II

--- topic/single_number_II/single_number_II.py
+++ topic/single_number_II/single_number_II.py
@@ -3,4 +3,16 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return 1
+        exist = {}
+
+        for num in nums:
+            if num in exist:
+                exist[num] += 1
+            else:
+                exist[num] = 1
+
+        for num, count in exist.items():
+            if count == 1:
+                return num
+
+        return 0
