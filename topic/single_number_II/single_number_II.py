17829acca34338bdb628a9644ed8eab15fe96c79
YoeaKai <cek2llm@gmail.com>
2022-12-23 23:05:29 +0800

    Add: single_number_II method 2

--- topic/single_number_II/single_number_II.py
+++ topic/single_number_II/single_number_II.py
@@ -1,8 +1,21 @@
 from typing import List
 
-
 class Solution:
+    # Method 1: Bit Manipulation.
     def singleNumber(self, nums: List[int]) -> int:
+        single = 0
+
+        for i in range(32):
+            count = 0
+            for num in nums:
+                if num & (1<<i) == (1<<i):
+                    count += 1
+            single |= (count%3) << i
+
+        return single if single < (1<<31) else single - (1<<32)
+
+    # Method 2
+    def singleNumber_2(self, nums: List[int]) -> int:
         exist = {}
 
         for num in nums:
