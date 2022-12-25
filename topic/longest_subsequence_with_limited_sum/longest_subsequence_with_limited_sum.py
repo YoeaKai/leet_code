ed8be2ac993373f7de78ea6f91d1f7f15d195a39
YoeaKai <cek2llm@gmail.com>
2022-12-26 00:13:06 +0800

    Add: longest_subsequence_with_limited_sum algorithm

--- topic/longest_subsequence_with_limited_sum/longest_subsequence_with_limited_sum.py
+++ topic/longest_subsequence_with_limited_sum/longest_subsequence_with_limited_sum.py
@@ -1,5 +1,15 @@
 from typing import List
+import bisect
 
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
-        return nums
+        nums.sort()
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]
+
+        res = []
+
+        for query in queries:
+            res.append(bisect.bisect_right(nums, query))
+
+        return res
