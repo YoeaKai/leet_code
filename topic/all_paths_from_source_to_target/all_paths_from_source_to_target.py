3a7ece4781637e9f145a17c8bafc0685e14d4f93
YoeaKai <cek2llm@gmail.com>
2022-12-31 00:32:05 +0800

    Add: all_paths_from_source_to_target algorithm

--- topic/all_paths_from_source_to_target/all_paths_from_source_to_target.py
+++ topic/all_paths_from_source_to_target/all_paths_from_source_to_target.py
@@ -3,4 +3,19 @@ from typing import List
 
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        return graph
+        if not graph:
+            return []
+
+        res = []
+        stack = [(0, [0])]
+
+        while stack:
+            node, path = stack.pop()
+
+            if node == len(graph) - 1:
+                res.append(path)
+
+            for next_node in graph[node]:
+                stack.append((next_node, path+[next_node]))
+
+        return res
