91458c8befe61a95e2d22ab6d3d1cd52c2851a83
YoeaKai <cek2llm@gmail.com>
2023-01-05 01:16:43 +0800

    Add: minimum_rounds_to_complete_all_tasks algorithm

--- topic/minimum_rounds_to_complete_all_tasks/minimum_rounds_to_complete_all_tasks.py
+++ topic/minimum_rounds_to_complete_all_tasks/minimum_rounds_to_complete_all_tasks.py
@@ -1,6 +1,8 @@
 from typing import List
+from collections import Counter
 
 
 class Solution:
     def minimumRounds(self, tasks: List[int]) -> int:
-        return 1
+        frequency = Counter(tasks).values()
+        return -1 if 1 in frequency else sum((f + 2) // 3 for f in frequency)
