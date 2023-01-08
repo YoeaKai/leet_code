857eab84cf56f67e6edc32a1190b719647e04c90
Yoea <cek2llm@gmail.com>
2023-01-08 23:35:05 +0800

    Add: linked_list_cycle algorithm

--- topic/linked_list_cycle/linked_list_cycle.py
+++ topic/linked_list_cycle/linked_list_cycle.py
@@ -9,4 +9,16 @@ class ListNode:
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+
+        slow = head
+        fast = head.next
+
+        while slow != fast:
+            if not fast or not fast.next:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+
         return True
