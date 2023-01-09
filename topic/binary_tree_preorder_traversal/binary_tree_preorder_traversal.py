559b676ee37b45d0444f3d0bc1ffa1671192bc7f
Yoea Kai <cek2llm@gmail.com>
2023-01-09 18:24:04 +0800

    Add: binary_tree_preorder_traversal

---
+++ topic/binary_tree_preorder_traversal/binary_tree_preorder_traversal.py
@@ -0,0 +1,30 @@
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+
+    def preorderTraversal2(self, root: Optional[TreeNode]) -> List[int]:
+        preorder = []
+
+        def dfs(node):
+            if not node:
+                return
+            preorder.append(node.val)
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+
+        return preorder
