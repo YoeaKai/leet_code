e828d73d88d943d53354ee28945504318cc9a814
Yoea <cek2llm@gmail.com>
2023-01-11 23:19:00 +0800

    Add: minimum_time_to_collect_all_apples_in_a_tree algorithm

--- topic/minimum_time_to_collect_all_apples_in_a_tree/minimum_time_to_collect_all_apples_in_a_tree.py
+++ topic/minimum_time_to_collect_all_apples_in_a_tree/minimum_time_to_collect_all_apples_in_a_tree.py
@@ -3,4 +3,25 @@ from typing import List
 
 class Solution:
     def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
-        return 1
+        graph = [[] for _ in range(n)]
+        for v1, v2 in edges:
+            graph[v1].append(v2)
+            graph[v2].append(v1)
+
+        visited = set()
+
+        def dfs(node):
+            if node in visited:
+                return 0
+
+            visited.add(node)
+
+            times = 0
+            for child in graph[node]:
+                times += dfs(child)
+            if times > 0:
+                return times + 2
+
+            return 2 if hasApple[node] else 0
+
+        return max(dfs(0) - 2, 0)
