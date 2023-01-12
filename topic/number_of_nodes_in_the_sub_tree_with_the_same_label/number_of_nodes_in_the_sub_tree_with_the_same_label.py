a001a1abddc82577c97bf84a4e341c9517adcd49
Yoea <cek2llm@gmail.com>
2023-01-13 01:24:24 +0800

    Add: number_of_nodes_in_the subtree algorithm

--- topic/number_of_nodes_in_the_sub_tree_with_the_same_label/number_of_nodes_in_the_sub_tree_with_the_same_label.py
+++ topic/number_of_nodes_in_the_sub_tree_with_the_same_label/number_of_nodes_in_the_sub_tree_with_the_same_label.py
@@ -1,6 +1,27 @@
 from typing import List
+from collections import Counter, defaultdict
 
 
 class Solution:
     def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
-        return None
+        def dfs(node: int, parent: int):
+            count = Counter(labels[node])
+
+            for child in graph.get(node, []):
+                if child != parent:
+                    count += dfs(child, node)
+
+            res[node] = count[labels[node]]
+
+            return count
+
+        res = [0] * n
+        
+        graph = defaultdict(list)
+        for a, b in edges:
+            graph[a] += [b]
+            graph[b] += [a]
+
+        dfs(0, -1)
+
+        return res 
