8205e7616624ef3413b61cd617a42cd3890d528e
Yoea <cek2llm@gmail.com>
2023-01-15 13:11:18 +0800

    Fix: lexicographically_smallest_equivalent_string blank line

--- topic/lexicographically_smallest_equivalent_string/lexicographically_smallest_equivalent_string.py
+++ topic/lexicographically_smallest_equivalent_string/lexicographically_smallest_equivalent_string.py
@@ -7,7 +7,7 @@ class Solution:
             if char != smaller_dict[char]:
                 smaller_dict[char] = find(smaller_dict[char])
             return smaller_dict[char]
-        
+
         def union(c1,c2):
             smallest1 = find(c1)
             smallest2 = find(c2)
@@ -16,7 +16,7 @@ class Solution:
                 smaller_dict[smallest1] = smallest2
             else:
                 smaller_dict[smallest2] = smallest1
-        
+
         for idx in range(len(s1)):
             union(s1[idx],s2[idx])
         
