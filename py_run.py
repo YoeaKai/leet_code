181c568f185ee76a0d8c78f25893ed5fd5962fc7
Yoea <cek2llm@gmail.com>
2023-01-16 00:51:03 +0800

    Add: number_of_good_paths algorithm

--- py_run.py
+++ py_run.py
@@ -1,9 +1,9 @@
-from topic.lexicographically_smallest_equivalent_string.lexicographically_smallest_equivalent_string import Solution
+from topic.number_of_good_paths.number_of_good_paths import Solution
 
 
 def run():
     solution = Solution()
-    print(solution.smallestEquivalentString("paaker", "morris", "parser"))
+    print(solution.numberOfGoodPaths([1,3,2,1,3], [[0,1],[0,2],[2,3],[2,4]]))
 
 
 if __name__ == '__main__':
--- topic/number_of_good_paths/number_of_good_paths.py
+++ topic/number_of_good_paths/number_of_good_paths.py
@@ -1,5 +1,43 @@
 from typing import List
+from collections import defaultdict
+from math import comb
 
 class Solution:
     def numberOfGoodPaths(self, vals: List[int], edges: List[List[int]]) -> int:
-      return 1
+        graph = {}
+
+        def find(node):
+            graph.setdefault(node, node)
+            if node != graph[node]:
+                graph[node] = find(graph[node])
+            return graph[node]
+
+        def union(node1 ,node2):
+            graph[find(node1)] = find(node2)
+        
+        tree = defaultdict(list)
+        val_node = defaultdict(set)
+        for node1, node2 in edges:
+            tree[node1].append(node2)
+            tree[node2].append(node1)
+            val_node[vals[node1]].add(node1)
+            val_node[vals[node2]].add(node2)
+
+        res = len(vals)
+
+        for val in sorted(val_node.keys()):
+            for node in val_node[val]:
+                for neighbors in tree[node]:
+                    if vals[neighbors] <= val:
+                        union(node,neighbors)
+
+            group_count = defaultdict(int)
+            for node in val_node[val]:
+                group_count[find(node)] += 1
+                
+            for root in group_count.keys():
+                # Calculate the combination of nodes that can connect, equal to:
+                # res += group_count[root] * (group_count[root]-1) // 2
+                res += comb(group_count[root], 2)
+        
+        return res
