0f5b5202755014e97bc0c66f974d81d2f3ef2207
Yoea <cek2llm@gmail.com>
2023-01-29 23:21:48 +0800

    Add: lfu_cache

--- topic/lfu_cache/lfu_cache.py
+++ topic/lfu_cache/lfu_cache.py
@@ -1,13 +1,51 @@
+from collections import defaultdict
+from collections import OrderedDict
+
+
 class LFUCache:
 
     def __init__(self, capacity: int):
-        return
+        self.capacity = capacity
+        self.minfreq = None
+        self.keyfreq = {}
+        self.freqkeys = defaultdict(OrderedDict)
 
     def get(self, key: int) -> int:
-        return
+        if key not in self.keyfreq:
+            return -1
+
+        freq = self.keyfreq[key]
+        val = self.freqkeys[freq][key]
+        # For update freqkeys with the frequence of the key.
+        del self.freqkeys[freq][key]
+
+        if not self.freqkeys[freq]:
+            if freq == self.minfreq:
+                self.minfreq += 1
+            del self.freqkeys[freq]
+
+        self.keyfreq[key] = freq+1
+        self.freqkeys[freq+1][key] = val
+
+        return val
 
     def put(self, key: int, value: int) -> None:
-        return
+        if self.capacity == 0:
+            return
+
+        if key in self.keyfreq:
+            freq = self.keyfreq[key]
+            self.freqkeys[freq][key] = value
+            self.get(key)
+            return
+
+        if len(self.keyfreq) == self.capacity:
+            delkey, _ = self.freqkeys[self.minfreq].popitem(last=False)
+            del self.keyfreq[delkey]
+
+        self.keyfreq[key] = 1
+        self.freqkeys[1][key] = value
+        self.minfreq = 1
 
         # Your LFUCache object will be instantiated and called as such:
         # obj = LFUCache(capacity)
