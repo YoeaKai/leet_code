61b955f6b17958ff35eff55044b7020b721b93f8
Yoea <cek2llm@gmail.com>
2023-01-29 23:11:55 +0800

    Add: lfu_cache

---
+++ topic/lfu_cache/lfu_cache.py
@@ -0,0 +1,15 @@
+class LFUCache:
+
+    def __init__(self, capacity: int):
+        return
+
+    def get(self, key: int) -> int:
+        return
+
+    def put(self, key: int, value: int) -> None:
+        return
+
+        # Your LFUCache object will be instantiated and called as such:
+        # obj = LFUCache(capacity)
+        # param_1 = obj.get(key)
+        # obj.put(key,value)
