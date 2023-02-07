418c5175e5fcbc2ef926d9d6ca09d50629801f57
YoeaKai <cek2llm@gmail.com>
2023-02-08 00:34:01 +0800

    Add: fruit_into_baskets

--- topic/fruit_into_baskets/fruit_into_baskets.py
+++ topic/fruit_into_baskets/fruit_into_baskets.py
@@ -3,4 +3,14 @@ from typing import List
 
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        return 1
+        count, start, cur = {}, 0, 0
+
+        for cur, fruit in enumerate(fruits):
+            count[fruit] = count.get(fruit, 0) + 1
+            if len(count) > 2:
+                count[fruits[start]] -= 1
+                if count[fruits[start]] == 0:
+                    del count[fruits[start]]
+                start += 1
+
+        return cur - start + 1
