600c26c77bbd84f77ba25c731c8112cb0d7a4f2c
Yoea <cek2llm@gmail.com>
2023-02-16 00:00:45 +0800

    Add: add_to_array_form_of_integer algorithm

--- topic/add_to_array_form_of_integer/add_to_array_form_of_integer.py
+++ topic/add_to_array_form_of_integer/add_to_array_form_of_integer.py
@@ -3,4 +3,6 @@ from typing import List
 
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        return num
+        for idx in range(len(num) - 1, -1, -1):
+            k, num[idx] = divmod(num[idx] + k, 10)
+        return [int(digit) for digit in str(k)] + num if k else num
