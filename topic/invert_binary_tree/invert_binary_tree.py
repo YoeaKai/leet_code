32704d7bc0bf7fe92f323f34a7ec8a6d24b97f28
Yoea <cek2llm@gmail.com>
2023-02-18 23:10:37 +0800

    Add: invert_binary_tree

---
+++ topic/invert_binary_tree/invert_binary_tree.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            if node != None:
+                node.left, node.right = node.right, node.left
+                stack += node.left, node.right
+
+        return root
