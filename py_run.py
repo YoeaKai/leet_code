60a910ce8d6a2ab763787d6b7c7dc8deaceb26ae
Yoea <cek2llm@gmail.com>
2023-02-22 23:49:25 +0800

    Add: capacity_to_ship_packages_within_d_days algorithm

--- py_run.py
+++ py_run.py
@@ -1,9 +1,9 @@
-from topic.single_element_in_a_sorted_array.single_element_in_a_sorted_array import Solution
+from topic.capacity_to_ship_packages_within_d_days.capacity_to_ship_packages_within_d_days import Solution
 
 
 def run():
     solution = Solution()
-    print(solution.singleNonDuplicate([1, 1, 2, 3, 3, 4, 4, 8, 8]))
+    print(solution.shipWithinDays([3, 2, 2, 4, 1, 4], 3))
 
 
 if __name__ == '__main__':
--- topic/capacity_to_ship_packages_within_d_days/capacity_to_ship_packages_within_d_days.py
+++ topic/capacity_to_ship_packages_within_d_days/capacity_to_ship_packages_within_d_days.py
@@ -3,4 +3,20 @@ from typing import List
 
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        return 1
+        left, right = int(max(weights)), sum(weights)
+
+        while left < right:
+            mid, count, cur = (left + right) // 2, 1, 0
+
+            for weight in weights:
+                if cur + weight > mid:
+                    count += 1
+                    cur = 0
+                cur += weight
+
+            if count > days:
+                left = mid + 1
+            else:
+                right = mid
+
+        return left
