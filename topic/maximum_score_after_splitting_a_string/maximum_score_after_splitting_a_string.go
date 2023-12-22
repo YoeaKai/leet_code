bca04adefc4e11415485ae595ca90162c1f46f5b
Yoea <cek2llm@gmail.com>
2023-12-23 01:48:06 +0800

    Add: maximum_score_after_splitting_a_string logic

--- topic/maximum_score_after_splitting_a_string/maximum_score_after_splitting_a_string.go
+++ topic/maximum_score_after_splitting_a_string/maximum_score_after_splitting_a_string.go
@@ -1,5 +1,28 @@
 package maximum_score_after_splitting_a_string
 
 func MaxScore(s string) int {
-	return 1
+	var countOne int
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == '1' {
+			countOne++
+		}
+	}
+
+	var maxScore int
+	cur := countOne
+
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] == '0' {
+			cur++
+		} else {
+			cur--
+		}
+
+		if cur > maxScore {
+			maxScore = cur
+		}
+	}
+
+	return maxScore
 }
